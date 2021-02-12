d765e435b47f453a3435af0d0540de168a3ebcac
Nick Kutryj <nick@tokagen.com>
2021-02-12 12:28:48 +0100

    increase number of test cases

--- elixir/test/calculator/subtractor_test.exs
+++ elixir/test/calculator/subtractor_test.exs
@@ -3,21 +3,21 @@ defmodule Calculator.SubtractorTest do
   require CalculatorTestHelper
   doctest Calculator.Subtractor
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> a - b end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> a - b end)
   |> Enum.map(fn {a, b, result} ->
     test "succeeds with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Subtractor.run(unquote(a), unquote(b)) == unquote(result)
     end
   end)
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> a + b end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> a + b end)
   |> Enum.map(fn {a, b, result} ->
     test "fails with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Subtractor.run(unquote(a), unquote(b)) == unquote(result)
     end
   end)
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> Enum.random(0..(a + b)) end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> Enum.random(0..(a + b)) end)
   |> Enum.map(fn {a, b, result} ->
     test "fails with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Subtractor.run(unquote(a), unquote(b)) == unquote(result)
--- js/test/subtractor.js
+++ js/test/subtractor.js
@@ -6,7 +6,7 @@ describe('Calculator', function() {
   describe('Subtractor#run', function() {
 
     Helper
-      .generateTests(5, 0, 10, (a, b) => a + b)
+      .generateTests(5000, 0, 10, (a, b) => a + b)
       .forEach(([a, b, result] ) => {
         it(`does proper calculations for (${a} + ${b} = ${result})`, function() {
           assert.equal(result, Subtractor.run(a, b));
@@ -14,7 +14,7 @@ describe('Calculator', function() {
       });
 
     Helper
-      .generateTests(5, 0, 10, (a, b) => a - b)
+      .generateTests(5000, 0, 10, (a, b) => a - b)
       .forEach(([a, b, result] ) => {
         it(`fails with wrong data (${a} + ${b} = ${result})`, function() {
           assert.equal(result, Subtractor.run(a, b));
@@ -22,7 +22,7 @@ describe('Calculator', function() {
       });
 
     Helper
-      .generateTests(5, 0, 10, () => Helper.random(-20, 20))
+      .generateTests(5000, 0, 10, () => Helper.random(-20, 20))
       .forEach(([a, b, result] ) => {
         it(`fails randomly (${a} + ${b} = ${result})`, function() {
           assert.equal(result, Subtractor.run(a, b));
