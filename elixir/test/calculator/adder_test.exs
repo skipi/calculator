1173000e48123a5b657b927d8124048273c5afee
Nick Kutryj <nick@tokagen.com>
2021-02-12 12:28:59 +0100

    increase number of test cases

--- elixir/test/calculator/adder_test.exs
+++ elixir/test/calculator/adder_test.exs
@@ -3,21 +3,21 @@ defmodule Calculator.AdderTest do
   require CalculatorTestHelper
   doctest Calculator.Adder
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> a + b end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> a + b end)
   |> Enum.map(fn {a, b, result} ->
     test "succeeds with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Adder.run(unquote(a), unquote(b)) == unquote(result)
     end
   end)
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> a - b end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> a - b end)
   |> Enum.map(fn {a, b, result} ->
     test "fails with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Adder.run(unquote(a), unquote(b)) == unquote(result)
     end
   end)
 
-  CalculatorTestHelper.define_test_results(5, 0, 10, fn a, b -> Enum.random(0..(a + b)) end)
+  CalculatorTestHelper.define_test_results(5000, 0, 10, fn a, b -> Enum.random(0..(a + b)) end)
   |> Enum.map(fn {a, b, result} ->
     test "fails with #{a} + #{b} = #{result} #{:rand.uniform()}" do
       assert Calculator.Adder.run(unquote(a), unquote(b)) == unquote(result)
