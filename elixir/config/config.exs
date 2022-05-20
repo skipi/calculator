228c76a303627b14ae3cafadd01ab49806abdf6d
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2022-05-20 15:00:20 +0200

    feat: playground setup

--- elixir/config/config.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-config :junit_formatter,
-  report_dir: "/tmp/test-results",
-  report_file: "junit.xml",
-  print_report_file: true,
-  prepend_project_name?: false,
-  include_filename?: true
